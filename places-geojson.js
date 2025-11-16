437e717b2fc1401d4fd89b6ce1dc4e1d14f4d7e9
Richard Aroksaar <raroksaar@me.com>
2025-11-16 08:46:52 -0800

    Add files via upload

--- places-geojson.js
+++ places-geojson.js
@@ -1,9 +1,32 @@
-
-// coords based on OpenSreetMap lookups of village and hamlet place names 
-// when exact location of manor is uncertain
 var square = {
     "type": "FeatureCollection",
     "features": [
+//        {
+ //           "type": "Feature",
+//           "geometry": {
+//				// Alatskivi   [S'58.5930199', N'58.6098346', E'27.1181135', W'27.1481482']
+//				//SE to SW to NW to NE to SE  
+//                "type": "LineString",
+//                "coordinates": [
+//				    // clockwise starting from South East point
+//                    [27.1181135, 58.5930199],
+					// vector, i.e line, from SE point to SW point
+//					[27.1481482, 58.5930199],
+					// vector from SW to NW
+//					[27.1481482, 58.6098346],
+					// vector from NW to NE
+//					[27.1181135, 58.6098346],
+					// vector from NE to SW
+//					[27.1181135, 58.5930199]
+					
+  //              ]
+//            },
+//            "properties": {
+//                'popupContent': 'Alatskivi<br><a href="https://www.mois.ee/tartu/alatskivi.shtml">Mõisa Poortal - Estonian Manors</a>',
+//                "underConstruction": false
+//            },
+//            "id": 1
+//        },
         {
             "type": "Feature",
             "geometry": {
@@ -47,10 +70,11 @@ var square = {
 };
 
 
-// coords based on Wikipedia lookups of manor names, 
-// using Estonian vikipeedia mostly, but also German wikipedia 
+
+
+
 // draws large circle
-var largecircle = {
+var approximate = {
     "type": "FeatureCollection",
     "features": [
 		    {
@@ -71,20 +95,8 @@ var largecircle = {
 			"geometry": {"coordinates": [25.47661, 58.850228], "type": "Point"}, "properties": {"popupContent": 'Kirna mõis<br><a href="https://www.mois.ee/jarva/kirna.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"
 			
 		},
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    26.138889,
-                    59.4225
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": '<b>Aaspere rüütlimõis</b><br><a href="https://www.mois.ee/viru/aaspere.shtml">Mõisa Poortal - Estonian Manors</a>'
-            },
-            "id": 52
-        },
+
+
 		    {
             "geometry": {
                 "type": "Point",
@@ -103,32 +115,6 @@ var largecircle = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-				27.387222, 59.3725
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": 'Edise mõis rüütlimõis Jõhvi kihelkonnas Virumaal<br><a href="https://www.mois.ee/viru/edise.shtml">Mõisa Poortal - Estonoan Manors</a> <br><a href="https://et.wikipedia.org/wiki/Edise_m%C3%B5is">Vikipeedia</a>'
-            },
-            "id": 58
-        },
-		{
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-				    25.328611, 59.248055
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": 'Alavere rüütlimõis <br><a href="https://www.mois.ee//harju/alavere.shtml">Mõisa Poortal - Estonoan Manors</a>'
-            },
-            "id": 55
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
                     26.057667,
                     59.070663
                 ]
@@ -139,29 +125,10 @@ var largecircle = {
             },
             "id": 54
         },
-		{
-			"geometry": {"coordinates": [24.777692, 58.6868], "type": "Point"}, "properties": {"Country: ": "Eesti", "popupContent": 'Kaisma rüütlimõis  Pärnu-Jaagupi kihelkonnas Pärnumaa<br><a href="https://et.wikipedia.org/wiki/Kaisma_m%C3%B5is">Vikipeedia</a>'}, "type": "Feature"
-			
-		},		
-		{
-
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    26.138889,
-                    59.4225
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": '<b>Aaspere rüütlimõis</b><br><a href="https://www.mois.ee/viru/aaspere.shtml">Mõisa Poortal - Estonian Manors</a>'
-            },
-            "id": 52
-        },
-			{"geometry": {"coordinates": [24.765067, 59.023431], "type": "Point"}, 
-			 "properties": {"Country: ": "Eesti", "popupContent": 'Alu rüütlimõis Rapla kihelkonnas Harjumaal<br><a href="https://www.mois.ee/harju/alu.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"
-			}			
-
+		
+  ///////
+    
+        
     ]
 };
 
@@ -169,20 +136,20 @@ var exactloc = {
 
     "type": "FeatureCollection",
     "features": [
-	//    {
-     //       "geometry": {
-     //           "type": "Point",
-     //           "coordinates": [
-	//				27.129707, 58.603866
+	    {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					27.129707, 58.603866
 
-      //          ]
-      //      },
-       //     "type": "Feature",
-       //     "properties": {
-       //         "popupContent": 'Alatskivi loss<br><a href="https://www.mois.ee/tartu/alatskivi.shtml">Mõisa Poortal - Estonian Manors</a>'
-       //     },
-       //     "id": 51
-       // },
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": 'Alatskivi loss<br><a href="https://www.mois.ee/tartu/alatskivi.shtml">Mõisa Poortal - Estonian Manors</a>'
+            },
+            "id": 51
+        },
 
         {
             "geometry": {
@@ -198,23 +165,7 @@ var exactloc = {
             },
             "id": 51
         },
-        {
-			//	var aaspere = L.marker([59.4225, 26.138889]).addTo(map)
-		//.bindPopup('<b>Aaspere rüütlimõis</b><br><a href="https://www.mois.ee/viru/aaspere.shtml">Mõisa Poortal - Estonian Manors</a>');
 
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    26.138889,
-                    59.4225
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": '<b>Aaspere rüütlimõis</b><br><a href="https://www.mois.ee/viru/aaspere.shtml">Mõisa Poortal - Estonian Manors</a>'
-            },
-            "id": 52
-        },
         {
             "geometry": {
                 "type": "Point",
@@ -229,7 +180,19 @@ var exactloc = {
             },
             "id": 54
         },
-        {
+		
+		{
+			"geometry": {"coordinates": [25.47661, 58.850228], "type": "Point"}, "properties": {"popupContent": 'Kirna mõis<br><a href="https://www.mois.ee/jarva/kirna.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"	
+		}
+
+	
+    ]
+};
+
+var knights = {
+    "type": "FeatureCollection",
+    "features": [
+			{
             "geometry": {
                 "type": "Point",
                 "coordinates": [
@@ -243,7 +206,20 @@ var exactloc = {
             "id": 55
         },
 
-        {
+			{
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.138889,
+                    59.4225
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+				 "popupContent": '<b>Aaspere rüütlimõis</b><br><a href="https://www.mois.ee/viru/aaspere.shtml">Mõisa Poortal - Estonian Manors</a>'
+            }
+        },
+		{
             "geometry": {
                 "type": "Point",
                 "coordinates": [
@@ -256,20 +232,16 @@ var exactloc = {
             },
             "id": 58
         },
-
 		{
 			"geometry": {"coordinates": [24.777692, 58.6868], "type": "Point"}, "properties": {"Country: ": "Eesti", "popupContent": 'Kaisma rüütlimõis  Pärnu-Jaagupi kihelkonnas Pärnumaa<br><a href="https://et.wikipedia.org/wiki/Kaisma_m%C3%B5is">Vikipeedia</a>'}, "type": "Feature"
+			
 		},
-		//{
-		//	"geometry": {"coordinates": [25.47661, 58.850228], "type": "Point"}, "properties": {"popupContent": 'Kirna mõis<br><a href="https://www.mois.ee/jarva/kirna.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"
-		//},
-			{"geometry": {"coordinates": [24.765067, 59.023431], "type": "Point"}, 
-			 "properties": {"Country: ": "Eesti", "popupContent": 'Alu rüütlimõis Rapla kihelkonnas Harjumaal<br><a href="https://www.mois.ee/harju/alu.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"
-		}		
-	
-    ]
-};
+		{
+			"geometry": {"coordinates": [24.765067, 59.023431], "type": "Point"}, "properties": {"Country: ": "Eesti", "popupContent": 'Alu rüütlimõis Rapla kihelkonnas Harjumaal<br><a href="https://www.mois.ee/harju/alu.shtml">Mõisa Poortal - Estonian Manors</a>'}, "type": "Feature"
+		},
 
+	]
+};
 
 
 
